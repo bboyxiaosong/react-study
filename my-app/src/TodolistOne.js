1688444bba24d63139863c2ea9df9bfc84a3c2cf
hanxs <bboyhxs@163.com>
2019-09-05 15:55:16 +0800

    hanxs

--- my-app/src/TodolistOne.js
+++ my-app/src/TodolistOne.js
@@ -3,6 +3,9 @@ import 'antd/dist/antd.css';
 import {Input,Button,List} from 'antd';
 import store from './store/index.js';
 
+import {getInputChangeAction,getAddItemAction,getDeleteItemAction} from './store/actionCreators';
+import {CHANGE_INPUT_VALUE,ADD_TODO_ITEM,DELETE_TODO_ITEM} from './store/actionType';
+
 // const data = [
 //     'Racing car sprays burning fuel into crowd.',
 //     'Japanese princess to wed commoner.',
@@ -17,21 +20,25 @@ class TodolistOne extends Component{
         this.state = store.getState()
         console.log(store.getState());
         // console.log(store);
+        this.handleStoreChange = this.handleStoreChange.bind(this);
         this.handleInputChange = this.handleInputChange.bind(this);
+        this.handleBtnClick = this.handleBtnClick.bind(this);
+        // this.handleItemDelete = this.handleItemDelete.bind(this);
+        store.subscribe(this.handleStoreChange);
     }
     render(){
         return (
         <div style={{marginLeft:'10px',marginTop:'10px'}}>
             <div>
                 <Input onChange={this.handleInputChange} value={this.state.inputValue} placeholder="todo info" style={{width:'300px',marginRight:'10px'}}/>
-                <Button>提交</Button>
+                <Button onClick={this.handleBtnClick}>提交</Button>
             </div>
             <List
             style={{width:'300px',marginTop:'10px'}}
             bordered
             dataSource={this.state.list}
-            renderItem={item => (
-                <List.Item>
+            renderItem={(item,index) => (
+                <List.Item onClick={this.handleItemDelete.bind(this,index)}>
                     {item}
                 </List.Item>
             )}
@@ -40,12 +47,34 @@ class TodolistOne extends Component{
         )
     }
     handleInputChange(e){
-        const action = {
-            'type':'change_input_value',
-            'value':e.target.value
-        }
+        // const action = {
+        //     'type':CHANGE_INPUT_VALUE,
+        //     'value':e.target.value
+        // }  
+        // 如果不引入配合 引入的 actionType 一起使用
+        const action = getInputChangeAction(e.target.value);
         store.dispatch(action)
     }
+    handleStoreChange(){
+        console.log('storgeCHange');
+        this.setState(store.getState());
+    }
+    handleBtnClick(){
+        // const action = {
+        //     type:ADD_TODO_ITEM
+        // }
+        const action = getAddItemAction();
+        store.dispatch(action);
+    }
+    handleItemDelete(index){
+        // const action = {
+        //     type:DELETE_TODO_ITEM,
+        //     index
+        // }
+        const action = getDeleteItemAction(index);
+        store.dispatch(action);
+    }
+
 
 }
 
--- my-app/src/store/reducer.js
+++ my-app/src/store/reducer.js
@@ -1,18 +1,31 @@
 
+import {CHANGE_INPUT_VALUE,ADD_TODO_ITEM,DELETE_TODO_ITEM} from './actionType';
 const defaultState = {
-    inputValue:'123',
-    list:[1,2]
+    inputValue:'',
+    list:[]
 };
-//reducer
+//reducer 可以接受 state 但是不能修改 state
 export default (state = defaultState,action)=>{
 
-    if(action.type === 'change_input_value'){
+    if(action.type === CHANGE_INPUT_VALUE){
         const newState = JSON.parse(JSON.stringify(state));
 
         newState.inputValue = action.value;
 
         return newState;
 
+    }
+
+    if(action.type === ADD_TODO_ITEM){
+        const newState = JSON.parse(JSON.stringify(state));
+        newState.list.push(newState.inputValue);
+        newState.inputValue = '';
+        return newState;
+    }
+    if(action.type === DELETE_TODO_ITEM){
+        const newState = JSON.parse(JSON.stringify(state));
+        newState.list.splice(action.index,1);
+        return newState;
     }
     return state;
 }
