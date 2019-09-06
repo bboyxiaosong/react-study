2449742662ac23c5af278567a6c6d26cf92d24e7
hanxs <bboyhxs@163.com>
2019-09-06 18:23:05 +0800

    hanxs

--- my-app/src/store/sagas.js
+++ my-app/src/store/sagas.js
@@ -6,17 +6,13 @@ import { initListAction } from './actionCreators';
 import axios from 'axios';
 
 function* getInitList(){
-
-    const res = yield axios.get('/api/todolist');
-    const action = initListAction(res.data);
-    yield put(action);
-    //  axios.get('/api/todolist')
-    //     .then((res)=>{
-    //         const data = res.data;
-    //         const action = initListAction(data)
-    //         put(action);
-    //     })
-    //     .catch(()=>{console.log('error')})
+    try{
+        const res = yield axios.get('/api/todolist');
+        const action = initListAction(res.data);
+        yield put(action);
+    }catch(e){
+        console.log('list.json 请求失败')
+    }
 
 }
 // generator
